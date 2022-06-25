494e113b71d2d84b68d1a796976a2d7ee75f5194
XiFeng <2748364315@qq.com>
2022-06-25 23:05:06 +0800

    some python code

---
+++ python/test.py
@@ -0,0 +1,9 @@
+a = input();
+b = input();
+c = input();
+print(a+' '+b+' '+c+'\n');
+print(a+'-'+b+'-'+c+'\n');
+print(a+'/'+b+'/'+c+'\n');
+print(b+','+c+','+a+'\n');
+print("{0}年{1}月{2}日".format(a,b,c)+'\n');
+print(a+'年'+b+'月'+c+'日'+'\n');
